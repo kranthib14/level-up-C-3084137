935639606223399a9766b0d2687758083f3f5cd4
kranthib14 <159272454+kranthib14@users.noreply.github.com>
2024-06-26 15:55:33 +0000

    done with first challenge by kk

--- src/01_01/ordinal_challenge.c
+++ src/01_01/ordinal_challenge.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
+char *ord[] = {"st", "nd", "rd", "th"};
 char *ordinal(int v)
 {
-	/*
-	   - create this function
-	   - this code won't run with out it!
-	 */
+	int rem = v % 10;
+	return ord[((rem < 4) && rem != 0) ? rem - 1 : 3];
 }
 
 int main()
@@ -13,16 +11,15 @@ int main()
 	int c;
 
 	/* output a table of 100 numbers */
-	for( c=1; c<=20; c++ )
+	for (c = 1; c <= 20; c++)
 	{
 		printf("%3d%s\t%3d%s\t%3d%s\t%3d%s\t%3d%s\n",
-				c, ordinal(c),
-				c+20, ordinal(c+20),
-				c+40, ordinal(c+40),
-				c+60, ordinal(c+60),
-				c+80, ordinal(c+80)
-			  );
+					 c, ordinal(c),
+					 c + 20, ordinal(c + 20),
+					 c + 40, ordinal(c + 40),
+					 c + 60, ordinal(c + 60),
+					 c + 80, ordinal(c + 80));
 	}
 
-	return(0);
+	return (0);
 }
